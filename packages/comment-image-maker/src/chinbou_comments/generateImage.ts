91eae7e8f0fb485afe220116841e1a0ca03d8a76
naoyashiga <>
2023-08-12 19:21:14 +0900

    update

--- packages/comment-image-maker/src/chinbou_comments/generateImage.ts
+++ packages/comment-image-maker/src/chinbou_comments/generateImage.ts
@@ -2,28 +2,40 @@ import puppeteer from 'puppeteer';
 import fs from 'fs';
 import path from 'path';
 
-async function generateCommentHTML(commentData: any): Promise<string> {
-  const date = new Date(commentData.time_parsed);
-  const formattedDate = `${date.getFullYear()}年${date.getMonth() + 1}月${date.getDate()}日 ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+async function generateCommentHTML(commentData: any, index: number): Promise<string> {
+  const commentDate = commentData.time_parsed 
+      ? new Date(commentData.time_parsed)
+      : null;
+  const videoPublishDate = new Date(commentData.Video.publish_date);
 
-  // テキスト内の「珍棒」をハイライトする
-  const highlightedText = commentData.text.replace(/珍棒/g, '<span style="background-color: yellow;">珍棒</span>');
+  const formattedCommentDate = commentDate 
+      ? `${commentDate.getFullYear()}年${commentDate.getMonth() + 1}月${commentDate.getDate()}日 ${commentDate.getHours()}:${commentDate.getMinutes()}:${commentDate.getSeconds()}`
+      : '';
+  const formattedVideoPublishDate = `${videoPublishDate.getFullYear()}年${videoPublishDate.getMonth() + 1}月${videoPublishDate.getDate()}日`;
 
+  const fontSize = commentData.text.length <= 60 ? '56px' : '24px';
+  const highlightedText = commentData.text.replace(/珍棒/g, `<span style="background-color: yellow; font-size: ${fontSize}; font-weight: bold;">珍棒</span>`);
   const thumbnailURL = `http://img.youtube.com/vi/${commentData.Video.video_id}/mqdefault.jpg`;
 
   return `
-      <div style="width: 800px; font-family: Arial, sans-serif; padding: 20px; box-sizing: border-box; display: flex; align-items: start; border: 1px solid #ccc; background-color: #ffffff;">
-          <img src="${commentData.AuthorChannel.photo}" style="width: 80px; height: 80px; border-radius: 50%; margin-right: 20px;">
-          <div style="flex: 1;">
-              <div style="display: flex; justify-content: space-between; align-items: center;">
-                  <span>${commentData.author_handle}</span>
-                  <span style="color: gray;">${formattedDate}</span>
+      <div style="width: 1920px; height: 1080px; font-family: Arial, sans-serif; background-color: #efefef; display: flex; justify-content: center; align-items: center; flex-direction: column;">
+          <div style="width: 80%; margin-bottom: 20px; font-size: 24px;">No.${index.toString().padStart(4, '0')}</div>
+          <div style="width: 80%; background-color: #ffffff; padding: 30px; box-sizing: border-box; display: flex; align-items: start; border: 1px solid #ccc;">
+              <img src="${commentData.AuthorChannel.photo}" alt="Profile Photo" style="width: 120px; height: 120px; border-radius: 50%; margin-right: 20px;">
+              <div>
+                  <div style="font-size: 24px;">${commentData.AuthorChannel.handle}</div>
+                  <div style="font-size: ${fontSize};">${highlightedText}</div>
+                  <div style="color: gray; margin-top: 10px;">
+                      ${formattedCommentDate}
+                  </div>
+                  <div style="font-size: 32px; margin-top: 10px;">${commentData.votes}いいね</div>
               </div>
-              <div style="font-size: 18px; margin-top: 10px;">${highlightedText}</div>
-              <div style="margin-top: 10px;">${commentData.votes}いいね</div>
-              <div style="margin-top: 10px; display: flex; align-items: center;">
-                  <img src="${thumbnailURL}" style="width: 80px; height: 45px; margin-right: 10px;">
-                  <span>${commentData.Video.title}</span>
+          </div>
+          <div style="width: 80%; background-color: #ffffff; padding: 30px; box-sizing: border-box; display: flex; align-items: center; border: 1px solid #ccc; margin-top: 20px;">
+              <img src="${thumbnailURL}" alt="Video Thumbnail" style="width: 240px; height: 135px; margin-right: 20px;">
+              <div>
+                  <div style="font-size: 32px;">${commentData.Video.title}</div>
+                  <div style="color: gray; margin-top: 5px;">${formattedVideoPublishDate}</div>
               </div>
           </div>
       </div>
@@ -31,12 +43,13 @@ async function generateCommentHTML(commentData: any): Promise<string> {
 }
 
 
-
-
-async function generateImage(commentData: any) {
-    const browser = await puppeteer.launch();
+async function generateImage(commentData: any, index: number): Promise<void> {
+    const browser = await puppeteer.launch({
+      headless: "new"
+    });
+  
     const page = await browser.newPage();
-    const htmlContent = await generateCommentHTML(commentData);
+    const htmlContent = await generateCommentHTML(commentData, index);
 
     await page.setContent(htmlContent);
     const element = await page.$('div');
@@ -52,7 +65,10 @@ async function generateImage(commentData: any) {
 
 (async () => {
     const resultsData = JSON.parse(fs.readFileSync(path.join(__dirname, 'results.json'), 'utf-8'));
-    for (let comment of resultsData.slice(0, 10)) {
-        await generateImage(comment);
+    const comments = resultsData.slice(0, 10);
+    // const comments = resultsData.slice(0, 10);
+    for (let i = 0; i < comments.length; i++) {
+        await generateImage(comments[i], i);
+        console.log(`Generated image for comment ${i}`)
     }
 })();
